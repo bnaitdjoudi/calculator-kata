b8468f5b5912c35fc6b6862914970449712b3d79
brahim <bnaitdjoudi@gmail.com>
2020-10-07 17:20:07 +0200

    etape 1

--- src/main/java/fr/enedis/kata/StringCalculator.java
+++ src/main/java/fr/enedis/kata/StringCalculator.java
@@ -3,6 +3,17 @@ package fr.enedis.kata;
 public class StringCalculator {
 
     public int add(String numbers) {
-        return -1;
+        if (numbers == null || "".equals(numbers)) {
+            return 0;
+        }
+        String[] nums = numbers.split(",");
+        int total = 0;
+        for (String item : nums) {
+            int num = Integer.parseInt(item);
+            total += num;
+        }
+        return total;
+
+
     }
 }
--- src/test/java/fr/enedis/kata/StringCalculatorTest.java
+++ src/test/java/fr/enedis/kata/StringCalculatorTest.java
@@ -1,7 +1,29 @@
 package fr.enedis.kata;
 
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
 
     // Right tests here
 
+    StringCalculator calculator = new StringCalculator();
+
+    @Test
+    public void emptyStringShouldReturnZero() {
+
+        assertEquals(calculator.add(""), 0," empty or null string should return 0");
+    }
+
+    @Test
+    public void oneNumberShouldReturnItSelf() {
+
+        assertEquals(calculator.add("3"), 3,"number should return it self");
+    }
+
+    @Test
+    public void twoNumberShouldAdded() {
+
+        assertEquals(calculator.add("1,2"), 3,"1,2 should return 3");
+    }
 }
